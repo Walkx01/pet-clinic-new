d11c2a6f683521f891fa72cbc5936a80e8d262b9
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-10-05 09:38:19 -0400

    Initial project added

---
+++ src/main/java/com/example/petclinicnew/PetClinicNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicNewApplication.class, args);
+    }
+
+}
